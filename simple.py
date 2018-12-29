8a4a7f80e46a7ab49ce56e523f0db758a513c281
Tong Du <tongdu@gmail.com>
2018-12-29 12:17:46 -0800

    use middleware for flask logging

--- simple.py
+++ simple.py
@@ -114,10 +114,24 @@ class Upload(Resource):
         file_path = os.path.join('/home/ubuntu/api-server/static', uploaded_file.filename)
         uploaded_file.save(file_path)
         url = url_for('static', filename=uploaded_file.filename)
-        str = pprint.pformat(request.environ, depth=5)
-        print str
         return {'url': url}, 201
 
 
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, resp):
+        errorlog = environ['wsgi.errors']
+        pprint.pprint(('REQUEST', environ), stream=errorlog)
+
+        def log_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(environ, log_response)
+
+
 if __name__ == '__main__':
+    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
     app.run(debug=True, host='0.0.0.0')
